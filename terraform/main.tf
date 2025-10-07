727d2a9437dec38084c0650c39c8531c621ff0e3
Amin <36745993+4Min4m@users.noreply.github.com>
2025-10-07 16:32:36 +0000

    corrected main.tf and added pvc.main

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,68 +4,44 @@ terraform {
       source  = "hashicorp/google"
       version = "~> 5.0"
     }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.0"
-    }
   }
 }
 
 provider "google" {
   project = "velvety-pagoda-474218-r6"
-  region  = "us-west1"
+  region  = "us-central1"
 }
 
-provider "kubernetes" {
-  host                   = "https://${google_container_cluster.bidflow_cluster.endpoint}"
-  token                  = data.google_client_config.default.access_token
-  cluster_ca_certificate = base64decode(google_container_cluster.bidflow_cluster.master_auth[0].cluster_ca_certificate)
-}
-
-data "google_client_config" "default" {}
-
+# Create GKE Autopilot Cluster
 resource "google_container_cluster" "bidflow_cluster" {
   name     = "bidflow-autopilot"
-  location = "us-west1-a"
+  location = "us-central1"
 
-  deletion_protection = false
-
-  remove_default_node_pool = true
-  initial_node_count       = 1
-
-  node_config {
-    machine_type = "e2-micro"
-    disk_type    = "pd-standard"
-    disk_size_gb = 20
-  }
+  enable_autopilot = true
 
   release_channel {
     channel = "REGULAR"
   }
+
+  deletion_protection = false
 }
 
-resource "kubernetes_namespace" "bidflow_ns" {
-  metadata {
-    name = "bidflow"
-  }
-
-  depends_on = [google_container_cluster.bidflow_cluster]
+# Output instructions
+output "cluster_name" {
+  value = google_container_cluster.bidflow_cluster.name
 }
 
-resource "kubernetes_persistent_volume_claim" "redis_pvc" {
-  metadata {
-    name      = "redis-pvc"
-    namespace = kubernetes_namespace.bidflow_ns.metadata[0].name
-  }
-  spec {
-    access_modes = ["ReadWriteOnce"]
-    storage_class_name = "standard" 
-    resources {
-      requests = {
-        storage = "1Gi"
-      }
-    }
-  }
-
-  depends_on = [kubernetes_namespace.bidflow_ns]
+output "setup_commands" {
+  value = <<-EOT
+    Run these commands after cluster is created:
+    
+    1. Configure kubectl:
+       gcloud container clusters get-credentials bidflow-autopilot --region=us-central1 --project=velvety-pagoda-474218-r6
+    
+    2. Create namespace:
+       kubectl create namespace bidflow
+    
+    3. Create PVC:
+       kubectl apply -f pvc.yaml
+  EOT
 }
